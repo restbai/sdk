a8a9a41ab4203797da0acd56313df6c2427705ce
Howard Hsieh <howard@restb.ai>
2018-08-14 16:00:37 +0200

    always pass in the `client_key` as a query parameter, even when using `base64` due to API-GW limitations

--- python/restb/sdk/api.py
+++ python/restb/sdk/api.py
@@ -6,7 +6,7 @@ from PIL import Image
 def service(url, endpoint, params):
     if params['image_base64'] is not None:
         params.pop('image_url', None)
-        return requests.post(url=url+endpoint, data=params, allow_redirects=False, timeout=60)
+        return requests.post(url=url+endpoint, params=dict(client_key=params['client_key']), data=params, allow_redirects=False, timeout=60)
     else:
         params.pop('image_base64', None)
         return requests.get(url=url+endpoint, params=params, allow_redirects=True, timeout=30)
